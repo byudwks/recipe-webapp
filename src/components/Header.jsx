829b1b1acf7ca1e8afd5ccd7ec2ad2f06f2df17e
byudwks <bayudwikiswanto24@gmail.com>
2026-06-03 15:26:22 +0700

    first commit recipe app

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="h-full py-5">
+      {/* navigation */}
+      <div className="">
+        
+      </div>
+    </header>
+  );
+};
+
+export default Header;
